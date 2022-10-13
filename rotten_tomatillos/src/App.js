102b72b9dad02ba925f5bf1da6e3fc98422bdaf5
Andrew Knapick <ajknapick@yahoo.com>
2022-10-13 18:37:45 -0500

    Added styling to application page

--- rotten_tomatillos/src/App.js
+++ rotten_tomatillos/src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
   render() { 
     return (
       <div className="App">
-        <h1>Rancid Tommatillos</h1>
+        <h1 className='App-header'>Rancid Tommatillos</h1>
         <Movies movies={this.state.movies}/>
       </div>
     )
