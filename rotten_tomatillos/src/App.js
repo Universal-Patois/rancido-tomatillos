673ec52a8ddf7eabcf975e1cf31b693d668345eb
Catalyst <103901063+Catalyst4Change@users.noreply.github.com>
2022-10-21 17:30:33 -0600

    jsjsj

--- rotten_tomatillos/src/App.js
+++ rotten_tomatillos/src/App.js
@@ -9,16 +9,17 @@ class App extends Component {
     super();
     this.state = {
       movies: [],
+      selectedMovie: '',
       error: '',
+    }
   }
-}
 
-componentDidMount = () => {
-  fetch('https://rancid-tomatillos.herokuapp.com/api/v2/movies')
-  .then(response => response.json())
-  .then(data => this.setState({movies: data.movies}))
-  .catch(error => this.setState({error: error.message}))
-}
+  componentDidMount = () => {
+    fetch('https://rancid-tomatillos.herokuapp.com/api/v2/movies')
+    .then(response => response.json())
+    .then(data => this.setState({movies: data.movies}))
+    .catch(error => this.setState({error: error.message}))
+  }
 
   render() { 
     return (
@@ -26,7 +27,7 @@ componentDidMount = () => {
         <h1 className='App-header'>Rancid Tomatillos</h1>
         {this.state.error && <h2>{this.state.error}</h2>}
         <Switch>
-          <Route exact path='/' render={() => <Movies className='Movies' movies={this.state.movies} selectMovie={this.selectMovie} /> } />
+          <Route exact path='/' render={() => <Movies className='Movies' movies={this.state.movies} /> } />
           <Route path='/:id' render={({ match }) => {
             const selectedMovie = this.state.movies.find(movie => movie.id === parseInt(match.params.id) )
             return <MovieDescription selectedMovie={selectedMovie} />
--- rotten_tomatillos/src/MovieDescription.js
+++ rotten_tomatillos/src/MovieDescription.js
@@ -1,25 +1,47 @@
-import React from "react";
+import React, { Component } from "react";
 import { Link } from 'react-router-dom';
 import './MovieDescription.css';
 
-const MovieDescription = ({ selectedMovie }) => {
-  if (!selectedMovie) {
-    return (
-      <div className="movie-info">Error 404: Movie ID does not exist.</div>
-    )
+class MovieDescription extends Component {
+  constructor({selectedMovie}) {
+    super()
+    this.state = {
+      id: selectedMovie.id,
+      title: selectedMovie.title,
+      backdrop_path: selectedMovie.backdrop_path,
+      poster_path: selectedMovie.poster_path,
+      average_rating: selectedMovie.average_rating,
+      release_date: selectedMovie.release_date
+    }
   }
-    return (
+
+  componentDidMount = () => {
+    const url = 'https://rancid-tomatillos.herokuapp.com/api/v2/movies/' + this.state.id
+    console.log(url);
+    fetch(url)
+    .then(response => response.json())
+    .then(data => console.log(data.movie))
+    .catch(error => this.setState({error: error.message}))
+    console.log(this.state.selectedMovie)
+  }
+  // if (!id) {
+  //   return (
+  //     <div className="movie-info">Error 404: Movie ID does not exist.</div>
+  //   )
+  // }
+  render() {
+        return (
       <div className="single-movie" 
            style={{
-            backgroundImage: `url(${selectedMovie.backdrop_path})`, 
+            backgroundImage: `url(${this.state.backdrop_path})`, 
             backgroundSize: '100vw',
           }}
           >
-        <img className='movie-image' src={selectedMovie.poster_path}></img>
+        <img className='movie-image' src={this.state.poster_path}></img>
         <div className="movie-info">
-          <h3>Title: {selectedMovie.title}</h3>
-          <h4 className="rating">Rating: {selectedMovie.average_rating}</h4>
-          <h4 className="release-date">Release Date: {selectedMovie.release_date}</h4>
+          <h3>Title: {this.state.title}</h3>
+          <h4 className="rating">Rating: {this.state.average_rating}</h4>
+          <h4 className="release-date">Release Date: {this.state.release_date}</h4>
           <Link to='/'>
             <button>Back</button>
           </Link>
@@ -27,6 +49,8 @@ const MovieDescription = ({ selectedMovie }) => {
         </div>
       </div>
     )
+
+  }
 }
 
 export default MovieDescription;
