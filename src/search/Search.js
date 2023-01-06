3978635bda406c31d7a605711a6e4399a2cdb103
Andrew Knapick <ajknapick@yahoo.com>
2023-01-06 13:38:45 -0600

    Finish autocomplete feature on search bar

--- src/search/Search.js
+++ src/search/Search.js
@@ -9,11 +9,10 @@ const Search = ({ movies }) => {
   const [value, setValue] = useState('')
 
   const handleChange = (event) => {
-    const query = event.target.value
+    const query = titleCase(event.target.value)
     setValue(query)
     if (query.length > 1) {
       const filterSuggestions = movies.filter(movie => movie.title.indexOf(query) > -1)
-      console.log(filterSuggestions)
       setSuggestions(filterSuggestions)
       setSuggestionActive(true)
     } else {
@@ -37,12 +36,18 @@ const Search = ({ movies }) => {
       if (suggestionIndex - 1 === suggestions.length) {
         return
       }
-      setSuggestionsIndex(setSuggestionsIndex + 1)
+      setSuggestionsIndex(suggestionIndex + 1)
     } else if (event.keyCode === 13) {
-      setValue(suggestions[suggestionIndex])
+      setValue(suggestions[suggestionIndex].title)
       setSuggestionsIndex(0)
       setSuggestionActive(false)
     }
+  }
+
+  const titleCase = (string) => {
+    return string.toLowerCase().split(' ').map((word) => {
+      return (word.charAt(0).toUpperCase() + word.slice(1));
+    }).join(' ');
   }
 
   const Suggestions = () => {
@@ -64,115 +69,68 @@ const Search = ({ movies }) => {
   }
 
   return (
-    <div className="autocomplete">
-      <input
-        type="text"
-        value={value}
-        onChange={handleChange}
-        onKeyDown={handleKeyDown}
-      />
-      {suggestionsActive && <Suggestions />}
-    </div>
+    <section className='filter-container'>
+      <div>
+        <input className='radio-button' type="radio" value="average_rating" name="sort" onChange={event => this.handleChange(event)} /> Sort by  Highest Rating
+      </div>
+      <div>
+        <input className='radio-button' type="radio" value="release_date" name="sort" onChange={event => this.handleChange(event)} /> Sort by Newest
+      </div>
+      <div>
+        <input
+          className='search-bar'
+          type="text"
+          value={value}
+          placeholder='Search for a movie here...'
+          onChange={handleChange}
+          onKeyDown={handleKeyDown}
+        />
+        <button className='search-button' onClick={event => this.submitSearch(event)}>Search</button>
+        {suggestionsActive && <Suggestions />}
+      </div>
+    </section>
   );
-
 }
 
 export default Search
 
-// class Search extends Component {
-//   constructor() {
-//     super()
-//     this.state = {
-//       query: '',
-//       error: false,
-//       sort: ''
-//     }
-//   }
 
-//   handleChange = (event) => {
-//     if (event.target.name === 'search') {
-//       this.setState({ query: event.target.value })
-//     } else if (event.target.value === 'average_rating') {
-//       this.setState({ ...this.state, sort: 'average_rating' })
-//       this.sortMoviesByRating()
-//     } else if (event.target.value === 'release_date') {
-//       this.setState({ ...this.state, sort: 'release_date' })
-//       this.sortMoviesByDate()
-//     }
-//   }
+{/* handleChange = (event) => {
+    if (event.target.name === 'search') {
+      this.setState({ query: event.target.value })
+    } else if (event.target.value === 'average_rating') {
+      this.setState({ ...this.state, sort: 'average_rating' })
+      this.sortMoviesByRating()
+    } else if (event.target.value === 'release_date') {
+      this.setState({ ...this.state, sort: 'release_date' })
+      this.sortMoviesByDate()
+    }
+  } */}
 
-//   submitSearch = (event) => {
-//     event.preventDefault()
-//     this.searchMovie()
-//     this.clearInput()
-//   }
+{/* submitSearch = (event) => {
+    event.preventDefault()
+    this.searchMovie()
+    this.clearInput()
+  } */}
 
-//   searchMovie = () => {
-//     const movie = this.props.movies.find(movie => {
-//       const foundMovie = movie.title === this.state.query
-//       if (foundMovie) {
-//         window.location.pathname = `/${movie.id}`
-//         return foundMovie
-//       }
-//     })
-//     movie ? this.props.addMovie(movie) : this.errorMessage()
-//   }
+{/* sortMoviesByRating = () => {
+    const sortedMovies = this.props.movies.sort((a, b) => {
+      return b.average_rating - a.average_rating
+    })
+    this.props.filterMovies(sortedMovies)
+  } */}
 
-//   sortMoviesByRating = () => {
-//     const sortedMovies = this.props.movies.sort((a, b) => {
-//       return b.average_rating - a.average_rating
-//     })
-//     this.props.filterMovies(sortedMovies)
-//   }
+{/* sortMoviesByDate = () => {
+    const sortedMovies = this.props.movies.map(movie => {
+      const newDates = new Date(movie.release_date)
+      movie.release_date = newDates
+      return movie
+    }).sort((a, b) => {
+      return b.release_date - a.release_date
+    })
+    this.props.filterMovies(sortedMovies)
+  } */}
 
-//   sortMoviesByDate = () => {
-//     const sortedMovies = this.props.movies.map(movie => {
-//       const newDates = new Date(movie.release_date)
-//       movie.release_date = newDates
-//       return movie
-//     }).sort((a, b) => {
-//       return b.release_date - a.release_date
-//     })
-//     this.props.filterMovies(sortedMovies)
-//   }
-
-//   clearInput = () => {
-//     this.setState({ query: '' })
-//   }
-
-//   errorMessage = () => {
-//     this.setState({ error: true })
-//     setTimeout(this.clearMessage, 4000)
-//   }
-
-//   clearMessage = () => {
-//     this.setState({ error: false })
-//   }
-
-//   render() {
-//     return (
-//       <section className='filter-container'>
-//         <div>
-//           <input type="radio" value="average_rating" name="sort" onChange={event => this.handleChange(event)} /> Sort by  Highest Rating
-//         </div>
-//         <div>
-//           <input type="radio" value="release_date" name="sort" onChange={event => this.handleChange(event)} /> Sort by Newest
-//         </div>
-//         <div>
-//           <input
-//             className='search-bar'
-//             type='text'
-//             name='search'
-//             value={this.state.query}
-//             placeholder='Search for a movie here'
-//             onChange={event => this.handleChange(event)}
-//           />
-//           <button className='search-button' onClick={event => this.submitSearch(event)}>Search</button>
-//           {this.state.error && <h2>Sorry! No movies were found. Please check that your spelling is correct and try again.</h2>}
-//         </div>
-//       </section>
-//     )
-//   }
-// }
-
-// export default Search;
+{/* clearInput = () => {
+    this.setState({ query: '' })
+  } */}
